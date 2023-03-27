7af881fc5f58d9070b3ab498852d30b8c5e75802
Lena Bertho <lena.bertho@protonmail.com>
2023-03-27 19:43:39 +0200

    ✨: implementation of, well, everything

--- example/main.cpp
+++ example/main.cpp
@@ -3,7 +3,44 @@
 #include <long_arrow/long_arrow.hpp>
 
 
+LONG_ARROW_GENERATE_OPERATOR_POST_DECREMENT(std::string)
+
+constexpr std::string concat_const(const std::string& str, char c)
+{
+	return str + c;
+}
+
+constexpr void concat_mut(std::string& str, char c)
+{
+	str += c;
+}
+
+constexpr std::string concat_rvalue(std::string&& str, char c)
+{
+	return std::move(str += c);
+}
+
+
+
 int main()
 {
-	std::cout << "Le cidre breton est meilleur que le cidre normand." << std::endl;
+	using long_arrow::operator>;
+
+	{
+		auto concat = LONG_ARROW_GENERATE_PROXY(concat_const);
+		std::string str = "Hell";
+		std::string res = str-->concat('o');
+		std::cout << res << std::endl; // Hello
+	}
+	{
+		auto concat = LONG_ARROW_GENERATE_PROXY(concat_mut);
+		std::string str = "Hell";
+		str-->concat('o');
+		std::cout << str << std::endl;
+	}
+	{
+		auto concat = LONG_ARROW_GENERATE_PROXY(concat_rvalue);
+		std::string str = std::string("Hell")-->concat('o');
+		std::cout << str << std::endl;
+	}
 }
--- lib/long_arrow/long_arrow.hpp
+++ lib/long_arrow/long_arrow.hpp
@@ -6,5 +6,81 @@
 
 namespace long_arrow 
 {
+/*
+** Proxy
+*/
+#define LONG_ARROW_GENERATE_PROXY(function)                                                                         \
+	[]<typename... Args>(Args&&... args)                                                                            \
+	{                                                                                                               \
+		return [&args...]<typename T>(T&& t) { return function(std::forward<T>(t), std::forward<Args>(args)...); }; \
+	}
+
+
+/*
+** Wrappers
+*/
+// For equivalent to const member function
+template <typename T>
+struct LongArrowObjectConstWrapper
+{
+	const T& wrapped;
+};
+
+// For equivalent to non const member function
+template <typename T>
+struct LongArrowObjectMutWrapper
+{
+	T& wrapped;
+};
+
+// For equivalent to member function with the rvalue ref-qualifier (the token && after the parameter list)
+template <typename T>
+struct LongArrowObjectMoveWrapper
+{
+	T&& wrapped;
+};
+
+/*
+** Help overload --(int) to return the 
+*/
+#define LONG_ARROW_GENERATE_OPERATOR_POST_DECREMENT(Type)                         \
+	long_arrow::LongArrowObjectConstWrapper<Type> operator--(const Type& t, int)  \
+	{                                                                             \
+		return long_arrow::LongArrowObjectConstWrapper<Type>{ .wrapped = t };     \
+	}                                                                             \
+                                                                                  \
+	long_arrow::LongArrowObjectMutWrapper<Type> operator--(Type& t, int)          \
+	{                                                                             \
+		return long_arrow::LongArrowObjectMutWrapper<Type>{ .wrapped = t };       \
+	}                                                                             \
+                                                                                  \
+	long_arrow::LongArrowObjectMoveWrapper<Type> operator--(Type&& t, int)        \
+	{                                                                             \
+		return long_arrow::LongArrowObjectMoveWrapper<Type>{ .wrapped = std::move(t) };      \
+	}                                                                             \
+
+
+/*
+** Overload of >
+*/
+template <typename T, std::invocable<T> Proxy>
+constexpr auto operator>(LongArrowObjectConstWrapper<T> wrapper, Proxy&& p)
+{
+	return p(wrapper.wrapped);
+}
+
+template <typename T, std::invocable<T&> Proxy>
+constexpr auto operator>(LongArrowObjectMutWrapper<T> wrapper, Proxy&& p)
+{
+	return p(wrapper.wrapped);
+}
+
+template <typename T, std::invocable<T&&> Proxy>
+constexpr auto operator>(LongArrowObjectMoveWrapper<T>&& wrapper, Proxy&& p)
+{
+	return p(std::move(wrapper.wrapped));
+}
+
+
 
 } // namespace long_arrow 
